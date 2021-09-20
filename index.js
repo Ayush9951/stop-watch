2feb5294b5f2bd9c8ec170cfb20739e42a753073
ayush9951 <ayushgupta9951@gmail.com>
2021-09-20 18:44:20 +0530

    Update UI

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let miliSeconds = 0
+let milliSeconds = 0
 let seconds = 0
 let minutes = 0
 let hours = 0
 
-let started = false
+let timeStarted = false
 
-let miliSecondsEl = document.getElementById("mili-seconds-el")
+let milliSecondsEl = document.getElementById("milli-seconds-el")
 let secondsEl = document.getElementById("seconds-el")
 let minutesEl = document.getElementById("minutes-el")
 let hoursEl = document.getElementById("hours-el")
@@ -14,40 +14,34 @@ let startBtn = document.getElementById("start-btn")
 let stopBtn = document.getElementById("stop-btn")
 let resetBtn = document.getElementById("reset-btn")
 
+//  when startTime() function is called for the first time, we set timeStarted = true
+//  bacause if by mistake we click the start button at the time when stop watch is already 
+//  running startTime() function will be called again so inorder to stop it from calling startTime() 
+//  function again and again we set timeStarted = true
 startBtn.addEventListener("click", function() {
-    // when startTime() function is called for the first time, we set the value started = true
-    // bacause if by mistake we click the start button at the time when stop watch is running
-    // this event listener will call the startTime() function again 
-    // so inorder to stop it from calling startTime() function again and again we set started = true
-    if (started === false) {
+    if (timeStarted === false) {
         startTime()
     }
 })
 
-stopBtn.addEventListener("click", function() {
-    stopTime()
-})
-
-resetBtn.addEventListener("click", function() {
-    resetTime()
-})
+stopBtn.addEventListener("click", stopTime)
+resetBtn.addEventListener("click", resetTime)
 
 let startId = undefined
 
+// startId = When setInterval function is called it will return an ID
 function startTime() {
-    started = true
-    startId = setInterval(function() {
-	    renderMiliSeconds()
-    }, 10)
+    timeStarted = true
+    startId = setInterval(renderMilliSeconds, 10)
 }
 
-function renderMiliSeconds() {
-    miliSeconds++
-    if (miliSeconds === 100) {
-        miliSeconds = 0
+function renderMilliSeconds() {
+    milliSeconds++
+    if (milliSeconds === 100) {
+        milliSeconds = 0
         renderSeconds()
     } else {
-        miliSecondsEl.textContent = miliSeconds
+        milliSecondsEl.textContent = milliSeconds
     }
 }
 
@@ -84,20 +78,20 @@ function renderHours() {
     }
 }
 
+// To clear the Interval or stop the time we will pass startId in clearInterval function
 function stopTime() {
-    started = false
+    timeStarted = false
     clearInterval(startId)
 }
 
 function resetTime() {
     stopTime()
-    miliSeconds = 0
+    milliSeconds = 0
     seconds = 0
     minutes = 0
     hours = 0
-    miliSecondsEl.textContent = "00"
+    milliSecondsEl.textContent = "00"
     secondsEl.textContent = "00"
     minutesEl.textContent = "00" + " : "
     hoursEl.textContent = "00" + " :"
 }
-
